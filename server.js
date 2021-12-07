71f854d8ff8359492c814e35cf56cc15e56856c1
algmoreno <alg.moreno00@gmail.com>
2021-12-07 12:04:06 -0700

    server running

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const { resolvers } = require('./schemas');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+async function startServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
 
-server.applyMiddleware({ app });
+  await server.start();  
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: false }))
+  app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-})
+});
+
+}
+
+startServer(); 
